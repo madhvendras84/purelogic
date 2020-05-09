39a6dcf67a0d504d3904cf5d50846c2427806e77
madhvendra <madhvendras84@gmail.com>
2020-05-09 18:22:02 +0530

    Minor changes in styling and responsiveness.

--- Parameter-ranges/assets/js/custom.js
+++ Parameter-ranges/assets/js/custom.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
-    });
-    
+        $('.sidebar-main').toggleClass('activenow');
+    });    
 });
+
+
+function hideShow(){
+    $('#hideShow').on('click',function(event){
+        // jQuery('body').find('#sidebarCollapse').trigger('click');        
+        $('#sidebar').removeClass('active');
+        $('.sidebar-main').removeClass('activenow');
+    });
+}
