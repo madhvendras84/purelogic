3028c1aabb630f390c19ae56e4eec54c1f498ed3
madhvendra <madhvendras84@gmail.com>
2020-05-12 18:07:25 +0530

    no message

---
+++ aqi-india-parameter-ranges/assets/js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('.sidebar-main').toggleClass('activenow');
+    });
+
+    $('a[href^="#"]').on('click', function (e) {
+        if (window.outerWidth > 768) {
+            var target = this.hash,
+                $target = $(target);
+            var xdistance = 68;
+            $('html, body').stop().animate({
+                'scrollTop': ($target.offset().top) - xdistance
+            }, 1000, '', function () {
+                window.location.hash = target;
+            });
+        }
+    });
+
+    $('#hideShow').on('click', function (event) {
+        $('#sidebar').removeClass('active');
+        $('.sidebar-main').removeClass('activenow');
+    });
+
+});
